beaeac03515d76737e345ae088b1edb4f752fefb
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2025-03-07 19:02:47 +0100

    add host deploy script

---
+++ build-host.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# remove old build directory
+script_dir="$(/usr/bin/realpath "$(/usr/bin/dirname "${BASH_SOURCE[0]}")")"
+build_dir="${script_dir}/build"
+output_dir="${script_dir}/output"
+/usr/bin/rm -r "${build_dir}"
+
+# create new build directory
+/usr/bin/mkdir -p "${build_dir}"
+# copy source files to build directory
+/usr/bin/cp -r "${script_dir}/SmartmonExporter" "${build_dir}"
+work_dir="${build_dir}/SmartmonExporter"
+# add new export stage to Dockerfile:
+#FROM scratch as export
+#COPY --from=publish /app/publish /
+echo "\n" >> "${work_dir}/Dockerfile"
+echo "FROM scratch as export" >> "${work_dir}/Dockerfile"
+echo "COPY --from=publish /app/publish /" >> "${work_dir}/Dockerfile"
+
+# build docker image with multi-stage build and export stage to output directory
+/usr/bin/docker build --target export -o "${output_dir}" "${work_dir}"
+
+# remove build directory
+/usr/bin/rm -r "${build_dir}"
